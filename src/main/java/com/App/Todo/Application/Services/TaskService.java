1825b89249b984b7cd0592bba7f1d8b05e9a8ac3
Abdelrahman Mohsen Ahmed <10812023100314@stud.cu.edu.eg>
2026-04-07 17:15:21 +0200

    Creating New Tasks

--- src/main/java/com/App/Todo/Application/Services/TaskService.java
+++ src/main/java/com/App/Todo/Application/Services/TaskService.java
@@ -3,6 +3,8 @@ package com.App.Todo.Application.Services;
 import com.App.Todo.Application.Models.Task;
 import com.App.Todo.Application.Repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,5 +16,12 @@ public class TaskService {
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
+    public void addTask(String title) {
+        Task newTask = new Task();
+        newTask.setTitle(title);
+        newTask.setCompleted(false);
+        taskRepository.save(newTask);
+    }
+
 
 }
--- src/main/java/com/App/Todo/Application/controllers/TaskController.java
+++ src/main/java/com/App/Todo/Application/controllers/TaskController.java
@@ -1,14 +1,13 @@
 package com.App.Todo.Application.controllers;
 
 import com.App.Todo.Application.Models.Task;
+import com.App.Todo.Application.Repositories.TaskRepository;
 import com.App.Todo.Application.Services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +16,20 @@ import java.util.List;
 public class TaskController {
     @Autowired
     private TaskService taskService;
+    @Autowired
+    private TaskRepository taskRepository;
+
     @GetMapping
     public ResponseEntity<String> getAllTasks(Model model) {
         List<Task> tasks = taskService.getAllTasks();
         model.addAttribute("tasks", tasks);
         return ResponseEntity.ok("Tasks");
     }
+    @PostMapping
+    public ResponseEntity<String> addTask(@RequestBody String title) {
+        taskService.addTask(title);
+        return ResponseEntity.status(HttpStatus.CREATED).body("redirect:/");
+    }
+
+
 }
